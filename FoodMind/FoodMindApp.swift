8d96f1e36e1f2768dba1ce6e37844f4807e01860
AhmadDurrani579 <ahmaddurranitrg@gmail.com>
2026-03-13 20:59:07 +0000

    Initial Commit

---
+++ FoodMind/FoodMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMindApp.swift
+//  FoodMind
+//
+//  Created by Ahmad on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FoodMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
